3bdf5c9b34cc0099f66286d534998ace579eb840
kusimplehan <Kushanwaghmare463@gmail.com>
2024-04-26 14:45:45 +0530

    Update RWG.py

--- RWG.py
+++ RWG.py
@@ -1,13 +1,26 @@
 import random
-i = input("Enter 'start' to begin: \n")
-while(i != "stop" ):
-    a = "abcdefghijklmnopqrstuvwxyz"
-    b = random.choice(a)
-    print(b)
-    i = input()
-    if i==b:
-        print(True)
-    elif(i=="stop"):
-        break
-    else:
-        print(False)
+type_style = input("single or multi : ")
+if type_style=="single":
+    n = input("Type a lettle : ")
+    while (type_style != "stop"):
+        print(n,"\n")
+        y = input()
+        if y==n:
+            print(True)
+        else:
+            print(False)
+
+elif type_style=="multi":
+
+    i = input("Enter 'start' to begin: \n")
+    while(i != "stop" ):
+        a = "abcdefghijklmnopqrstuvwxyz"
+        b = random.choice(a)
+        print(b,"\n")
+        i = input()
+        if i==b:
+            print(True)
+        elif(i=="stop"):
+            break
+        else:
+            print(False)
